5fa5ad77316dd803456865baf0963eab891ce94f
user135601 <user135601@103-246-99-74.moboware.com>
2018-05-07 02:24:01 +1000

    new

---
+++ BubblePop/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  BubblePop
+//
+//  Created by Abdol Vatandoust on 26/4/18.
+//  Copyright © 2018 Abdol Vatandoust. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var playerName: UITextField!
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var timeCount: UILabel!
+    @IBOutlet weak var bubbleSlider: UISlider!
+    @IBOutlet weak var bubbleCount: UILabel!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     }
+    
+    @IBAction func timeSlide(_ sender: UISlider) {
+        timeCount.text = String(Int(sender.value))
+     }
+    
+    @IBAction func bubbleSlide(_ sender: UISlider) {
+        bubbleCount.text = String(Int(sender.value))
+    }
+    
+    @IBAction func play(_ sender: Any) {
+
+        let gameRecord = playerName.text! + timeCount.text! + bubbleCount.text!
+        let gameFileName = "GameFile"
+        let rwt = ReadWriteFile()
+        rwt.writeFile(writeString: gameRecord, to: gameFileName)
+        print(rwt.readFile(from: gameFileName))
+    }
+
+    
+}
+
---
+++ FileHandling.h
@@ -0,0 +1,39 @@
+//
+//  FileHandling.h
+//  BubblePop
+//
+//  Created by user135601 on 5/6/18.
+//  Copyright © 2018 Abdol Vatandoust. All rights reserved.
+//
+
+#ifndef FileHandling_h
+#define FileHandling_h
+class ReadWriteFile{
+    var DocumentDirURL:URL{
+        let url = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return url
+    }
+    func fileURL(fileName:String,fileExtension:String) -> URL {
+        return DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension(fileExtension)
+    }
+    func writeFile(writeString:String, to fileName:String, fileExtension:String = "txt"){
+        let url = fileURL(fileName: fileName, fileExtension: fileExtension)
+        do{
+            try writeString.write(to: url,atomically: true, encoding: .utf8)
+        } catch let error as NSError {
+            print("Failed writing to URL: \(url), Error: " + error.localizedDescription)
+        }
+    }
+    func readFile(from fileName:String, fileExtension:String = "txt") ->String{
+        var readString = ""
+        let url = fileURL(fileName: fileName, fileExtension: fileExtension)
+        do{
+            readString = try String(contentsOf: url)
+        } catch let error as NSError {
+            print("Failed reading to URL: \(url), Error: " + error.localizedDescription)
+        }
+        return readString
+    }
+}
+
+#endif /* FileHandling_h */
